9d2c076cc8942286daab2dff0db6feeaa6462c1b
Gory-git <61806971+Gory-git@users.noreply.github.com>
2026-05-24 11:44:55 +0200

    aggiunta todo list, forse ce ne sono altri non committati sul fisso, vedremo

--- src/main.cpp
+++ src/main.cpp
@@ -11,3 +11,14 @@ void setup()
 void loop() 
 {
 }
+
+/* TODO list
+ * rendere una libreria
+ * scrivere una versione non bloccante che gira in background
+ * valutare l'aggiunta dei parametri di configurazione da linea di comando 
+ * migliorare parsing
+ * migliorare validazione
+ * rendere modulare <-- fatto
+ * aggiungere wear leveling (rotazione degli indirizzi di scrittura per una maggiore durata della eeprom)
+ * controllare sicurezza del codice (buffer o stack oerflow, stringhe ecc.)
+ */
